f0d87bc2b3877afc88b7e91c3a2f2011e5ab6d2a
73880591 <jacob.la@hotmail.com>
2021-02-04 10:51:43 +0100

    Add project files.

---
+++ Opgave 5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ØvelserLektion1
+{
+    class Program
+    {
+        public static double Max(params double[] arr)
+        {
+            double res = 0;
+            foreach (var item in arr)
+            {
+                if (item > res)
+                {
+                    res = item;
+                }
+
+            }
+            return res;
+
+        }
+
+
+        static void Main(string[] args)
+
+        {
+            double m1 = Max(28.5, 17.2);
+            double m2 = Max(4.0, 10.8, 34.25, 2.0, 23.6);
+
+            Console.WriteLine(m1 + " " + m2);
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ TestProjekt/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestProjekt
+{
+    class Program
+    {
+        static void Main()
+        {
+            int myInt = 0;
+            string navn = "jacob";
+            bool tr = true;
+
+            var minVar = 17;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine($"Tallet er nu {i}");
+            //}
+
+            string[] strA = new String[6];
+            int[] intA = { 5, 56, -100, 18, 89 };
+            String[] strB = { "52", "hund", "sune", "ost", "89"};
+
+            List<int> lst = new List<int>();
+            lst.Add(1);
+            lst.Add(7);
+            lst.Add(345);
+            lst.Add(6456);
+            
+            lst.ForEach(Console.WriteLine);
+
+            if (true)
+                Console.WriteLine();
+            else
+                Console.WriteLine();
+
+
+            //foreach (var item in lst)
+            //{
+            //    Console.WriteLine($"Elementet i vores array er {item}");
+            //}
+
+        }
+    }
+}
